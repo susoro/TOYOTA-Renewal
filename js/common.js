2650e9f392bdf6228ba7567b05956ba6536d5c2b
jenn <susoro567@gmail.com>
2021-03-31 20:58:49 +0900

    토글이 문제였다.... 😭😭😭
    슬라이더가 제대로 작동하고 있다.
    기적이다 이건 ... 빨리 끝내자!!

--- js/common.js
+++ js/common.js
@@ -5,4 +5,14 @@ $(document).ready(function () {
 $(".mv").bxSlider({
     auto: true
 });
+
 });
+
+
+// toggle
+function toggleMenu(){
+  const menuToggle = document.querySelector('.toggle');
+  const sidebar = document.querySelector('.sidebar');
+  menuToggle.classList.toggle('active');
+  sidebar.classList.toggle('active');
+}
