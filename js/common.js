1a2ddc07cddec686f34d31fe5306901993ed5565
jenn <susoro567@gmail.com>
2021-04-01 15:20:58 +0900

    footer 구현완료....

--- js/common.js
+++ js/common.js
@@ -6,7 +6,26 @@ $(document).ready(function () {
   });
 
 });
+//스크롤트리거
+$(window).scroll(function () {
+  $('.ani_stop').each(function (i) {
+    var bottom_of_object = $(this).offset().top + $(this).outerHeight() * .3;
+    var bottom_of_window = $(window).scrollTop() + $(window).height();
+    if (bottom_of_window > bottom_of_object) {
+      $(this).removeClass('ani_stop');
+    }
+    if (bottom_of_window < bottom_of_object) {
+      $(this).addClass('ani_stop');
+    }
+  });
+});
 
+// 모바일메뉴 
+$('.m_menu').click(function () {
+  $('.mgnb_wrap').animate({
+    left: '0'
+  });
+});
 
 // // toggle
 // function toggleMenu() {
