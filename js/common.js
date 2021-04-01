0ed65001908a5c77d3753cfc288c91f21e33fc16
jenn <susoro567@gmail.com>
2021-04-01 15:34:49 +0900

    😭😭😭😭😭😭😭😭

--- js/common.js
+++ js/common.js
@@ -6,19 +6,6 @@ $(document).ready(function () {
   });
 
 });
-//스크롤트리거
-$(window).scroll(function () {
-  $('.ani_stop').each(function (i) {
-    var bottom_of_object = $(this).offset().top + $(this).outerHeight() * .3;
-    var bottom_of_window = $(window).scrollTop() + $(window).height();
-    if (bottom_of_window > bottom_of_object) {
-      $(this).removeClass('ani_stop');
-    }
-    if (bottom_of_window < bottom_of_object) {
-      $(this).addClass('ani_stop');
-    }
-  });
-});
 
 // 모바일메뉴 
 $('.m_menu').click(function () {
@@ -26,13 +13,3 @@ $('.m_menu').click(function () {
     left: '0'
   });
 });
-
-// // toggle
-// function toggleMenu() {
-//   const menuToggle = document.querySelector('.toggle');
-//   const sidebar = document.querySelector('.sidebar');
-//   menuToggle.classList.toggle('active');
-//   sidebar.classList.toggle('active');
-// }
-
-// 탭메뉴
